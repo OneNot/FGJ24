639f8e7b0c8a1b818687d8f434200cbadc974461
Toni Hannula <toni.hannula@xamk.fi>
2024-01-27 00:04:43 +0200

    Basic Input system for gameplay

--- Assets/Character/Scripts/CharacterInputManager.cs
+++ Assets/Character/Scripts/CharacterInputManager.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterInputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField]
+    private InputActionAsset inputActionAsset;
 
+    private InputActionMap gameplayActionMap;
+
+    private InputAction moveAction;
+
+    private void Awake()
+    {
+        gameplayActionMap = inputActionAsset.FindActionMap("gameplay");
+        moveAction = gameplayActionMap.FindAction("move");
+        gameplayActionMap.FindAction("vault").performed += OnVault;
+        gameplayActionMap.FindAction("dash").performed += OnDash;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        Vector2 moveVector = moveAction.ReadValue<Vector2>();
+        if (moveVector.magnitude > 0f)
+        {
+            Debug.Log("MOVE: " + moveVector);
+        }
+    }
 
+    void OnEnable()
+    {
+        gameplayActionMap.Enable();
+    }
+    void OnDisable()
+    {
+        gameplayActionMap.Disable();
+    }
+
+
+    private void OnVault(InputAction.CallbackContext callbackContext)
+    {
+        Debug.Log("VAULT");
+    }
+
+    private void OnDash(InputAction.CallbackContext callbackContext)
+    {
+        Debug.Log("DASH");
     }
 }
