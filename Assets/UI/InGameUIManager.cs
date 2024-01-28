3db6a385adc27e8a63945342fbd9406a9d08e77c
Toni Hannula <toni.hannula@xamk.fi>
2024-01-28 14:33:06 +0200

    UI updates: lifebar

--- Assets/UI/InGameUIManager.cs
+++ Assets/UI/InGameUIManager.cs
@@ -4,10 +4,15 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class InGameUIManager : MonoBehaviour
 {
+    [SerializeField]
+    private Transform lifeBar;
+    [SerializeField]
+    private GameObject lifeIconPrefab;
     [SerializeField]
     private Image dashIcon, vaultIcon;
 
@@ -16,17 +21,21 @@ public class InGameUIManager : MonoBehaviour
 
     private float gameTimeElapsed = 0f, gameTimeStartTimeStamp = 0f;
 
+    private bool isTimerActive = false;
+
     private void Awake()
     {
-
     }
 
     private void Update()
     {
-        gameTimeElapsed = Time.time - gameTimeStartTimeStamp;
-        float seconds = (float)Math.Round(gameTimeElapsed % 60, 2);
-        int minutes = (int)gameTimeElapsed / 60;
-        timeTextIndicator.text = "Time: " + minutes.ToString("00") + ":" + seconds.ToString().Replace(".", ":").Replace(",", ":");
+        if (isTimerActive)
+        {
+            gameTimeElapsed = Time.time - gameTimeStartTimeStamp;
+            float seconds = (float)Math.Round(gameTimeElapsed % 60, 2);
+            int minutes = (int)gameTimeElapsed / 60;
+            timeTextIndicator.text = "Time: " + minutes.ToString("00") + ":" + seconds.ToString().Replace(".", ":").Replace(",", ":");
+        }
     }
 
     public void SetDashState(bool setEnabled)
@@ -43,5 +52,19 @@ public class InGameUIManager : MonoBehaviour
     {
         gameTimeElapsed = 0f;
         gameTimeStartTimeStamp = Time.time;
+        isTimerActive = true;
+    }
+    public void StopGameTimer()
+    {
+        isTimerActive = false;
+    }
+
+    public void SetLifeAmount(int howManyLeft)
+    {
+        foreach (Transform child in lifeBar)
+            Destroy(child.gameObject);
+
+        for (int i = 0; i < howManyLeft; i++)
+            Instantiate(lifeIconPrefab, lifeBar);
     }
 }
