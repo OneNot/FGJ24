b55d2ed63591123100fc2c75b01d9841c7ea53fc
Toni Hannula <toni.hannula@xamk.fi>
2024-01-28 16:04:29 +0200

    Main menu working start and quit

--- Assets/UI/MainmenuNav.cs
+++ Assets/UI/MainmenuNav.cs
@@ -9,23 +9,30 @@ public class MainmenuNav : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] GameObject credits;
     // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            LoadLevel(1);
-        }
-        if(Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            credits.SetActive(!credits.activeInHierarchy);
-        }
-        if(Input.GetKeyDown(KeyCode.Mouse2))   
-        {
-            Application.Quit();
-        }
-    }
+    // void Update()
+    // {
+    //     if(Input.GetKeyDown(KeyCode.Mouse0))
+    //     {
+    //         LoadLevel(1);
+    //     }
+    //     if(Input.GetKeyDown(KeyCode.Mouse1))
+    //     {
+    //         credits.SetActive(!credits.activeInHierarchy);
+    //     }
+    //     if(Input.GetKeyDown(KeyCode.Mouse2))   
+    //     {
+    //         Application.Quit();
+    //     }
+    // }
 
-    void LoadLevel(int i){
-        SceneManager.LoadScene(i);
+    public void StartGame()
+    {
+        Debug.Log("Start Game");
+        SceneManager.LoadScene(1);
+    }
+    public void Quit()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
     }
 }
